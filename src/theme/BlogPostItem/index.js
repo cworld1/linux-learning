2c3c8cf93aa3c38079857681bdc9f947835b9cc3
cworld1 <cworld0@qq.com>
2022-11-23 21:01:18 +0800

    feat(swizzle): :zap: add blog comment support

---
+++ src/theme/BlogPostItem/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BlogPostItem from "@theme-original/BlogPostItem";
+import Comments from "../../components/Comments";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+
+export default function BlogPostItemWrapper(props) {
+  const { isBlogPostPage } = useBlogPost();
+
+  return (
+    <>
+      <BlogPostItem {...props} />
+      {isBlogPostPage && <Comments {...{ type: "blog" }} />}
+    </>
+  );
+}
